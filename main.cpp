1d76b3f1cae55855cbe459f33729de3d64e7f17a
Dilec <dilec.padovani@gmail.com>
2022-11-11 23:19:07 +0100

    fix

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 
 
-
 #include <iostream>
 using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
-int a,b,c;
-cin >> a >> b >> c;
+int main()
+{
+    // Qui vanno definite le variabili
+    int a, b, c;
+    cin >> a >> b >> c;
 
-float area_triangolo = 0.5*a*b;
-float area_quadrato = a*a;
-float area_rettangolo = a*b;
-float area_trapezio = 0.5*( (a+b)*c );
+    cout << a << endl
+         << b << endl
+         << c << endl;
 
-cout << area_triangolo << endl << area_quadrato << endl << area_rettangolo << endl << area_trapezio ;
+    float area_triangolo = 0.5 * a * b;
+    float area_quadrato = a * a;
+    float area_rettangolo = a * b;
+    float area_trapezio = 0.5 * ((a + b) * c);
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+    cout << area_triangolo << endl
+         << area_quadrato << endl
+         << area_rettangolo << endl
+         << area_trapezio;
 
-
+    // Qui va scritto il codice per eseguire i calcoli richiesti
 }
